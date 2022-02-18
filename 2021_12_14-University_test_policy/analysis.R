6f063842977783de541ad734fd1906ccb705d32e
Restrepo <LRestrepo@urban.org>
2022-02-18 14:49:04 -0500

    updating script and data

--- 2021_12_14-University_test_policy/analysis.R
+++ 2021_12_14-University_test_policy/analysis.R
@@ -30,119 +30,95 @@ rm(list = ls())
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 ## Tuition
-years <- c(2004:2019)
+ys = 2004:2019
 
-for(i in 1:length(years)){
-  df.temp <- get_education_data(level="college-university",
-                                source = "ipeds",
-                                topic="academic-year-tuition",
-                                filters=list(year=years[i]),
-                                add_labels=TRUE)
-  df.temp <- df.temp[which(df.temp$level_of_study=="Undergraduate" & df.temp$tuition_type=="In state"),]
-  df.temp <- df.temp[c("unitid", "year", "tuition_type", "tuition_fees_ft")]
-  if(i == 1){
-    tuition <- df.temp}
-  else{tuition <- rbind(tuition, df.temp)}
-}
+tuition = data.table::fread("https://educationdata.urban.org/csv/ipeds/colleges_ipeds_ay_tuition_fees.csv")%>%
+  filter(
+    level_of_study==1,
+    tuition_type==3,
+    year%in%ys)%>%
+  select(unitid, year, tuition_type, tuition_fees_ft)
 
 #Directory
-years <- c(2004:2019)
+ys = 2004:2019
 
-for(i in 1:length(years)){
-  df.temp <- get_education_data(level="college-university",
-                                source = "ipeds",
-                                topic="directory",
-                                filters=list(year=years[i]),
-                                add_labels=TRUE)
-  df.temp <- df.temp[c("unitid", "year", "inst_name", 
-                       "address", "state_abbr", "zip","offering_highest_level",
-                       "region", "inst_control", "institution_level", 
-                       "sector", "fips", "hbcu")]
-  if(i == 1){
-    directory <- df.temp}
-  else{directory <- rbind(directory , df.temp)}
-  print(paste(years[i], "has been added to the Directory"))
-}
+directory = data.table::fread("https://educationdata.urban.org/csv/ipeds/colleges_ipeds_directory.csv")%>%
+  filter(
+    year%in%ys)%>%
+  select("unitid", "year", "inst_name", 
+         "address", "state_abbr", "zip","offering_highest_level",
+         "region", "inst_control", "institution_level", 
+         "sector", "fips", "hbcu")
 
 #Enrollment
-years <- c(2004:2018)
+ys = 2004:2018
 
-for(i in 1:length(years)){
-  df.temp <- get_education_data(level="college-university",
-                                source = "ipeds",
-                                topic="admissions-enrollment",
-                                filters=list(year=years[i]),
-                                add_labels=TRUE)
-  df.temp <- df.temp[which(df.temp$sex=="Total"),]
-  df.temp <- df.temp[c("unitid", "year", "number_applied", "number_admitted", "number_enrolled_ft")]
-  if(i == 1){
-    enroll <- df.temp}
-  else{enroll <- rbind(enroll, df.temp)}
-}
+enroll = data.table::fread("https://educationdata.urban.org/csv/ipeds/colleges_ipeds_admissions-enrollment.csv")%>%
+  filter(
+    year%in%ys,
+    sex==99)%>%
+  select("unitid", "year", "number_applied", "number_admitted", "number_enrolled_ft")
 
 ## SAT and ACT Scores
-years <- c(2004:2018)
-for(i in 1:length(years)){
-  df.temp <- get_education_data(level="college-university",
-                                source = "ipeds",
-                                topic="admissions-requirements",
-                                filters=list(year=years[i]),
-                                add_labels=TRUE)
-  df.temp <- df.temp[c("year", "unitid", "open_admissions_policy","sat_crit_read_75_pctl", "sat_math_75_pctl", "act_composite_75_pctl")]
-  df.temp$year <- years[i]
-  if(i == 1){
-    testscores <- df.temp}
-  else{testscores <- rbind(testscores , df.temp)}
-}
+years = 2004:2018
+testscores = data.table::fread("https://educationdata.urban.org/csv/ipeds/colleges_ipeds_admissions-requirements.csv")%>%
+  filter(
+    year%in%ys)%>%
+  select("year", "unitid", "open_admissions_policy","sat_crit_read_75_pctl", "sat_math_75_pctl", "act_composite_75_pctl")
 
 #set aside open enrollment
-open <- testscores[c("year", "unitid", "open_admissions_policy")]
-open <- open[which(open$year==2018),]
+open <- testscores%>%
+  select("year", "unitid", "open_admissions_policy")
+open <- open[open$year==2018,]
 testscores$open_admissions_policy <- NULL
 
 ## Merge data
 
-data <- merge(directory, tuition, by=c("unitid", "year"), all.x=TRUE, all.y=TRUE)
-data <- merge(data, enroll, by=c("unitid", "year"), all.x=TRUE, all.y=TRUE)
-data <- merge(data, testscores, by=c("unitid", "year"), all.x=TRUE, all.y=TRUE)
+data <- merge(directory, tuition, by=c("unitid", "year"), all=TRUE)
+data <- merge(data, enroll, by=c("unitid", "year"), all=TRUE)
+data <- merge(data, testscores, by=c("unitid", "year"), all = TRUE)
 
 ## Drop Private For-Profit Institutions
-forprofit <- unique(data$unitid[data$inst_control=="Private for-profit"])
-
-data <- data[!(data$unitid %in% forprofit),]
+data <- data[!(data$unitid %in% data$unitid[data$inst_control==3]),]
 
 ## Interpolate and Copy for NAs
 
-data1 <- NULL
+data$check_2 = rowSums(!is.na(data[,15:21]))>0
+
+data1 = data %>%
+  group_by(unitid)%>%
+  mutate(check_1 = n()==16)%>%
+  filter(check_1 == T,
+         check_2 == T)
 
 for(i in 1:length(unique(data$unitid))){
-  usedata <- data[which(data$unitid==unique(data$unitid)[i]),]
+  usedata <- data1[which(data1$unitid==unique(data1$unitid)[i]),]
   usedata <- rbind(usedata, usedata[nrow(usedata),])
-  if(sum(!is.na(usedata[,15:21]))>0 & nrow(usedata[,15:21])==17){
-    usedata$year[nrow(usedata)] <- 2020
-    usedata[,15:21] <- na.approx(usedata[,15:21], method="linear", rule=2, na.rm=FALSE)
-    if(i==1){
-      data1 <- usedata
-    }else{
-      data1 <- rbind(data1, usedata)
-    }}else{
-      data1 <- data1
-    }
+  usedata$year[nrow(usedata)] = 2020
+  if(i==1){
+    data1_a <- usedata
+  }else{
+    data1_a <- rbind(data1_a, usedata)
+  }
 }
-
-schools <- read.csv("Data/Input_Data/schools.csv") #Read in adopters
+data1 = data1_a%>%
+  mutate(across(14:20,~na.approx(.,method="linear",rule = 2, na.rm=F)))%>%
+  select(-c(check_1,check_2))
+  
+schools <- readr::read_csv("Data/Input_Data/schools.csv") #Read in adopters
 
 data1$censor <- 0
 data1$censor[!(data1$unitid %in% unique(schools$unitid))] <- 1
 
 adopters <- data1[which(data1$censor==0),]
-censored <- data1[which(data1$censor==1),]
 
-censored <- censored[which(censored$year==2020),]
+censored <- data1[data1$censor==1,]
+censored <- censored[censored$year==2020,]
+
 censored$school <- censored$permenant <- censored$temp_year <- censored$GPA <- censored$blind <- NA
+censored$left_censor <- 0
 
-adopters <- merge(adopters, schools, by=c("unitid", "year"), all.x=FALSE, all.y=FALSE)%>%
-  rename(school = `ï..school`)
+adopters <- merge(adopters, schools, by=c("unitid", "year"), all = FALSE)
 
 data2 <- rbind(adopters,censored)
 
@@ -159,7 +135,7 @@ data2$selectivity <- data2$number_admitted/data2$number_applied*100
 
 # Remove Open Enrollment Schools
 
-data2 <- data2[!(data2$unitid %in% open$unitid[open$open_admissions_policy=="Yes"]),]
+data2 <- data2[!(data2$unitid %in% open$unitid[open$open_admissions_policy==1]),]
 
 #Create indicator of COVID adoption
 data2$covid <- 0
@@ -174,11 +150,27 @@ data2$label <- "Non-Adopter"
 data2$label[data2$censor==0 & data2$covid==0] <- "Pre-COVID"
 data2$label[data2$censor==0 & data2$covid==1] <- "COVID"
 
+# Calculate tuition adjusted based on the Higher Education Price Index
+#https://www.commonfund.org/higher-education-price-index
+
+year <- c(2004:2020)
+hepi.values <- c(231.7, 240.8, 253.1, 260.3, 273.2, 279.3, 281.8, 288.4, 293.2, 297.8, 
+                 306.7, 312.9, 317.7, 327.4, 336.1, 346.0, 352.7)
+hepi <- as.data.frame(cbind(year, hepi.values))
+
+data2 <- merge(data2, hepi, by="year")
+data2$tuition_real <- data2$tuition_fees_ft*352.7/data2$hepi.values #Calcualted in 2020 dollars
+
 #####################################################
 ################# Data Analysis #####################
 #####################################################
 
 data <- data2
+
+# Counts by category
+nrow(data[which(data$label=="Pre-Covid"),])
+nrow(data[which(data$label=="COVID"),])
+nrow(data[which(data$label=="Non-Adopter"),])
 
 # Generating the table on pg 1
 data3 = data2 %>%
@@ -187,22 +179,22 @@ data3 = data2 %>%
 
 data4 = data2 %>%
   group_by(label)%>%
-  summarise(enrollment = mean(tuition_fees_ft, na.rm=T))
+  summarise(tuition = mean(tuition_real, na.rm=T))
 
 data5_1 = data2 %>%
   group_by(label)%>%
-  summarise(enrollment = mean(selectivity, na.rm=T))
+  summarise(selectivity = mean(selectivity, na.rm=T))
 
 library(car)
 
 ## Average Tuition
 
-leveneTest(tuition_fees_ft ~ label, data=data)
-oneway.test(tuition_fees_ft ~ label, data=data, var.equal=FALSE)
+leveneTest(tuition_real ~ label, data=data)
+oneway.test(tuition_real ~ label, data=data, var.equal=FALSE)
 
-pairwise.t.test(data$tuition_fees_ft, data$label, p.adjust.method="BH", pool.sd=FALSE)
+pairwise.t.test(data$tuition_real, data$label, p.adjust.method="BH", pool.sd=FALSE)
 
-aggregate(data$tuition_fees_ft, list(data$label), FUN=mean, na.rm=TRUE, na.action=na.pass)
+aggregate(data$tuition_real, list(data$label), FUN=mean, na.rm=TRUE, na.action=na.pass)
 
 ##Selectivity
 #Admissions Rate
@@ -215,9 +207,9 @@ pairwise.t.test(data$selectivity, data$label, p.adjust.method="BH", pool.sd=FALS
 aggregate(data$selectivity, list(data$label), FUN=mean, na.rm=TRUE, na.action=na.pass)
 
 ##HBCU
-nrow(data[which(data$hbcu=="Yes"),]) #62
-nrow(data[which(data$hbcu=="Yes" & data$censor==0 & data$covid==0),]) #1
-1/62
-nrow(data[which(data$hbcu=="Yes" & data$censor==0 & data$covid==1),]) #19
+nrow(data[which(data$hbcu==1),]) #62
+nrow(data[which(data$hbcu==1 & data$censor==0 & data$covid==0),]) #32
+32/62
+nrow(data[which(data$hbcu==1 & data$censor==0 & data$covid==1),]) #19
 19/62
-20/62
+51/62
